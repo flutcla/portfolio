5d4a2087fa9012cbfb2a2825f5f31ceb08a4de30
Kosuke <mail@r.flutcla.tech>
2023-05-22 10:04:48 +0900

    refactored name

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { FaGithub } from 'react-icons/fa';
-import { Color } from "../color";
+import { Color } from "../Color";
 
 interface HeaderProps {
   title: String
