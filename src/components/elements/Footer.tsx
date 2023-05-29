f037335b5943f729edb32bf0f87d1679d932f9d9
Kosuke <mail@r.flutcla.tech>
2023-05-29 09:39:03 +0900

    added last update

--- src/components/elements/Footer.tsx
+++ src/components/elements/Footer.tsx
@@ -3,14 +3,22 @@ import { styled } from "styled-components"
 export const Footer = () => {
   return (
     <SFooterContainer>
-      <div>&copy; 2023 FlutCla</div>
+      <SFooterText><SFooterSpan>&copy; 2023 FlutCla</SFooterSpan><SFooterSpan>Last Update: {lastUpdated}</SFooterSpan></SFooterText>
     </SFooterContainer>
   )
 }
+
+const lastUpdated = process.env.REACT_APP_BUILD_DATE || "[ビルド時の最終更新日]"
 
 const SFooterContainer = styled.footer`
   position: absolute;
   bottom: 0;
   width: 100%;
   margin: 10px;
+`;
+
+const SFooterText = styled.div``;
+
+const SFooterSpan = styled.span`
+  margin: 0 10px;
 `;
